1722747f10c2fdc7b4dcf652524f0963077f12cf
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2021-01-19 21:24:04 +0300

    Adding Node API setup

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// DB
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected!'))
+  .catch((err) => console.log('MongoDB connection error: ', err));
+
+const app = express();
+
+// Routes
+const blogRoutes = require('./routes/blog');
+
+// Middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use('/api', blogRoutes);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
