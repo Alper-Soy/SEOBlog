cb8e6892a60ed2ec763e77b0e9f27457ad3d20dd
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2021-01-22 17:06:09 +0300

    Adding category page

--- client/pages/admin/index.js
+++ client/pages/admin/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Admin from '../../components/auth/Admin';
 import Layout from '../../components/Layout';
 
@@ -5,7 +6,23 @@ const AdminIndex = () => {
   return (
     <Layout>
       <Admin>
-        <h2>Admin Dashboard</h2>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-md-12 pt-5 pb-5'>
+              <h2>Admin Dashboard</h2>
+            </div>
+            <div className='col-md-4'>
+              <ul className='list-group'>
+                <li className='admin-index list-group-item'>
+                  <Link href='/admin/crud/category-tag'>
+                    <a className='admin-index-a'>Create Category</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className='col-md-8'>right</div>
+          </div>
+        </div>
       </Admin>
     </Layout>
   );
