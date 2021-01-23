19db8d24fa18c40c7c1d53de70d5e15e88aa9696
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2021-01-23 14:52:47 +0300

    Adding tag component

--- client/pages/admin/crud/category-tag.js
+++ client/pages/admin/crud/category-tag.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Admin from '../../../components/auth/Admin';
 import Category from '../../../components/crud/Category';
+import Tag from '../../../components/crud/Tag';
 import Layout from '../../../components/Layout';
 
 const CategoryTag = () => {
@@ -13,10 +14,12 @@ const CategoryTag = () => {
               <h2>Manage Categories and Tags</h2>
             </div>
             <div className='col-md-6'>
+              <h4 className='text-secondary'>Categories</h4>
               <Category />
             </div>
             <div className='col-md-6'>
-              <p>Tags</p>
+              <h4 className='text-secondary'>Tags</h4>
+              <Tag />
             </div>
           </div>
         </div>
