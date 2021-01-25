56a6e7bd6b1d5c091c6f753766aedefd5d1ebad6
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2021-01-25 13:42:03 +0300

    Showing categories and tags in checkbox

--- client/components/crud/BlogCreate.js
+++ client/components/crud/BlogCreate.js
@@ -23,6 +23,12 @@ const CreateBlog = ({ router }) => {
     }
   };
 
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+
+  const [checked, setChecked] = useState([]); // categories
+  const [checkedTag, setCheckedTag] = useState([]); // tags
+
   const [body, setBody] = useState(blogFromLS());
   const [values, setValues] = useState({
     error: '',
@@ -41,14 +47,52 @@ const CreateBlog = ({ router }) => {
     title,
     hidePublishButton,
   } = values;
+  const token = getCookie('token');
 
   useEffect(() => {
     setValues({ ...values, formData: new FormData() });
+    initCategories();
+    initTags();
   }, [router]);
+
+  const initCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  const initTags = () => {
+    getTags().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setTags(data);
+      }
+    });
+  };
 
   const publishBlog = (e) => {
     e.preventDefault();
-    console.log('ready to publishBlog');
+    // console.log('ready to publishBlog');
+    createBlog(formData, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({
+          ...values,
+          title: '',
+          error: '',
+          success: `A new blog titled "${data.title}" is created`,
+        });
+        setBody('');
+        setCategories([]);
+        setTags([]);
+      }
+    });
   };
 
   const handleChange = (name) => (e) => {
@@ -60,12 +104,75 @@ const CreateBlog = ({ router }) => {
 
   const handleBody = (e) => {
     // console.log(e);
-    console.log(formData);
     setBody(e);
     formData.set('body', e);
     if (typeof window !== 'undefined') {
       localStorage.setItem('blog', JSON.stringify(e));
     }
+  };
+
+  const handleToggle = (c) => () => {
+    setValues({ ...values, error: '' });
+    // return the first index or -1
+    const clickedCategory = checked.indexOf(c);
+    const all = [...checked];
+
+    if (clickedCategory === -1) {
+      all.push(c);
+    } else {
+      all.splice(clickedCategory, 1);
+    }
+    console.log(all);
+    setChecked(all);
+    formData.set('categories', all);
+  };
+
+  const handleTagsToggle = (t) => () => {
+    setValues({ ...values, error: '' });
+    // return the first index or -1
+    const clickedTag = checked.indexOf(t);
+    const all = [...checkedTag];
+
+    if (clickedTag === -1) {
+      all.push(t);
+    } else {
+      all.splice(clickedTag, 1);
+    }
+    console.log(all);
+    setCheckedTag(all);
+    formData.set('tags', all);
+  };
+
+  const showCategories = () => {
+    return (
+      categories &&
+      categories.map((c, i) => (
+        <li key={i} className='list-unstyled'>
+          <input
+            onChange={handleToggle(c._id)}
+            type='checkbox'
+            className='mr-2'
+          />
+          <label className='form-check-label'>{c.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  const showTags = () => {
+    return (
+      tags &&
+      tags.map((t, i) => (
+        <li key={i} className='list-unstyled'>
+          <input
+            onChange={handleTagsToggle(t._id)}
+            type='checkbox'
+            className='mr-2'
+          />
+          <label className='form-check-label'>{t.name}</label>
+        </li>
+      ))
+    );
   };
 
   const createBlogForm = () => {
@@ -101,12 +208,65 @@ const CreateBlog = ({ router }) => {
   };
 
   return (
-    <div>
-      {createBlogForm()}
-      <hr />
-      {JSON.stringify(title)}
-      <hr />
-      {JSON.stringify(body)}
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-8'>
+          {createBlogForm()}
+          <hr />
+          {JSON.stringify(title)}
+          <hr />
+          {JSON.stringify(body)}
+          <hr />
+          {JSON.stringify(categories)}
+          <hr />
+          {JSON.stringify(tags)}
+        </div>
+
+        <div className='col-md-4'>
+          <div>
+            <div className='form-group pb-2'>
+              <h5>Featured image</h5>
+              <hr />
+
+              <small className='text-muted mr-2'>Max size: 1mb</small>
+              <label className='btn btn-outline-info'>
+                Upload featured image
+                <input
+                  onChange={handleChange('photo')}
+                  type='file'
+                  accept='image/*'
+                  hidden
+                />
+              </label>
+            </div>
+          </div>
+          <div>
+            <h5>Categories</h5>
+            <hr />
+
+            <ul
+              style={{
+                maxHeight: '200px',
+                overflow: 'auto',
+              }}
+            >
+              {showCategories()}
+            </ul>
+          </div>
+          <div>
+            <h5>Tags</h5>
+            <hr />
+            <ul
+              style={{
+                maxHeight: '200px',
+                overflow: 'auto',
+              }}
+            >
+              {showTags()}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
